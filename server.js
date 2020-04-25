bc28f0d2293b593a0280767cf50cf3d4c038d395
Igor Prates <Igor@clinicatotal.com.br>
2020-04-25 10:39:39 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+//tratativa da rota
+const express =require('express');
+//padrao do node, para encontrar as pastas, etc
+const path = require('path');
+
+//cria o app, com o expresse que é a rota de onde ele vai ser encontrado
+const app = express();
+
+/*cria o server definindo o protodolo http do app que é a porta que vais er
+acessada pelo websocket*/
+const server = require('http').createServer(app);
+//define o protocolo wss para o socketio, passando o serveer como function
+const io = require("socket.io")(server);
+
+//define uma pasta de onde vai ficar os arquivos publicos da aplicacao
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*Configuraçoes para mostrar para o app que vou usar as 'views' como html,
+que por padrao no node usa ejs(é a engine tem no v8 que é escutada no node
+via express )*/
+//define onde vai ficar as views
+app.set("views", path.join(__dirname, 'public'));
+//define a engine como html
+app.engine('html', require('ejs').renderFile);
+//agora posso usar html nas minhas views
+app.set('view engine', 'html');
+
+//quando acessar o endereço incial do meu server '/' vai renderizar o index.html
+app.use('/', (req,res)=>{
+  res.render('index.html');
+});
+
+//chama o server na porta 3000 'localhos:3000'
+server.listen('3000');
+
